7f30efa14cf58203a8333f1491b2a03da82505c1
mr-suida <mr.suida@outlook.com>
2024-07-18 22:46:29 -0300

    start get parse function

--- libs/http_parse.lua
+++ libs/http_parse.lua
@@ -1,4 +1,6 @@
 
+local http_headers = require 'http_heads'()
+
 local http_parse = function()
     return {
         request_line = function(buffer)
@@ -12,10 +14,19 @@ local http_parse = function()
             return nil
         end,
         
-        get_parse = function(client_socket)
-            local req,res = nil, nil
-
-            return req,res 
+        get_parse = function(client_socket,path)
+            local req = http_headers.req_schm;
+            local res = http_headers.res_schm;
+            
+            while true do
+                local buff = client_socket('*l')
+                if #buff == 0 then break end 
+                local key,value = string.match(read_line,'(%S+)%s*:%s*(%S+)')
+                
+                if k ~= nil and v ~= nil then
+                    req.headers[string.lower(k)] = v
+                end
+            end 
         end
     }
 end
