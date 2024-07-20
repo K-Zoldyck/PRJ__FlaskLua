3bdfe2b3b2a9f6e93fb41b4668da44d7c90f1826
mr-suida <mr.suida@outlook.com>
2024-07-19 21:14:23 -0300

    new get test

--- test.lua
+++ test.lua
@@ -5,9 +5,10 @@ local app = flask_lua()
 
 
 app:get('/all',function(req,res)
+    -- http://127.0.0.1:3000/all?name=your name
     print(req.parameters['name']) 
-    res.buff = 'ola, mundo'
-    res:set_head('Content-Type','text/plain')
+    res.buff = '<p>ola,'..req.parameters['name']..'</p>'
+    res:set_head('Content-Type','text/html')
     
     return res
 end)
